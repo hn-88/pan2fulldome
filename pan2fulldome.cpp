b2eed3ea444eec82d73f8f6de11c1caa4b8092ac
hn-88 <hn-88@users.noreply.github.com>
2023-07-09 17:04:28 +0530

    Size is in cv namespace

--- pan2fulldome.cpp
+++ pan2fulldome.cpp
@@ -130,7 +130,7 @@ cv::Mat dst2, dst3, dsts;	// temp dst, for eachvid
 	
 	outputw = atoi(lTmp);
 	cv::Mat img = cv::imread(escapedpath, cv::IMREAD_COLOR);
-	cv::resize(img, dst, Size(400,400), 0, 0, cv::INTER_CUBIC); 
+	cv::resize(img, dst, cv::Size(400,400), 0, 0, cv::INTER_CUBIC); 
 	
 	if(img.empty())
 		 {
