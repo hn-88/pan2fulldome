59254c8c4079bfe3e3d22498ea8d07ae32d236a7
hn-88 <hn-88@users.noreply.github.com>
2023-08-28 20:58:33 +0530

    correction to y in copyTo()

--- pan2fulldome.cpp
+++ pan2fulldome.cpp
@@ -62,7 +62,7 @@ cv::Mat simplePolar(cv::Mat inputMat, int sky_threshold, int horizontal_extent, 
 	cv::resize(inputMat, tmp, cv::Size(horizontal_extent, outputw-sky_threshold), 0, 0, cv::INTER_CUBIC);
 	//tmp.rowRange(1, outputw-sky_threshold).copyTo(dst.rowRange(sky_threshold+1, outputw));
 	int x =  (int)(outputw-horizontal_extent)/2;
-	int y =  outputw-sky_threshold;
+	int y =  sky_threshold;
 	tmp.copyTo(dst(cv::Rect(x,y,tmp.cols, tmp.rows)));
 	
 	cv::Point2f centrepoint( (float)dst.cols / 2, (float)dst.rows / 2 );
